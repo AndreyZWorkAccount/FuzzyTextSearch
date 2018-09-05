58786e832ea9e5129bc39335380d1fb0560c6c1d
AndreyZWorkAccount <42203416+AndreyZWorkAccount@users.noreply.github.com>
2018-09-05 13:17:09 +0300

    preliminary changes

---
+++ priorityQueue/priorityQueue.go
@@ -0,0 +1,22 @@
+//Copyright Copyright 2018 Andrey Z.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+
+package priorityQueue
+
+
+
+type IPriorityQueue interface {
+	Insert(Prioritized)
+	Pop()    	Prioritized
+	Peek()    	Prioritized
+	Size()		int
+}
+
+type Prioritized interface {
+	Priority() int
+}
